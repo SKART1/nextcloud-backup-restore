aa0e7c5ded11bf2b0daa6912f305f5bf8b256952
Art <art.enikoff@gmail.com>
2019-05-04 02:08:11 +0300

    fixes in restore script

--- ncrestore.sh
+++ ncrestore.sh
@@ -60,7 +60,6 @@ echo
 # Restore the files from borg archive
 # 
 info "Doing..."
-cd ${extractTempDir}
 cd ${extractTempDir} && borg extract -v --list ::"${borg_archive}"
 echo "Deleting old Nextcloud data directory... And copying new one"
 rm -r "${nextcloudDataDir}"
