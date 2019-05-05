41d0e0160102ee4a239ac2ac19aaed8c1a751666
Art <art.enikoff@gmail.com>
2019-05-05 14:08:18 +0300

    created new script

--- nccopy.sh
+++ nccopy.sh
@@ -25,8 +25,8 @@ for file in "$BORG_REPO"/data/0/*; do
     if [ -L "$file" ]; then
         continue;
     else
-        mv ./$file ${target_directory}/data/0/
-        ln -s ${target_directory}/data/0/$file ./$file
+        cp $file ${target_directory}/data/0/
+        #ln -s ${target_directory}/data/0/$file ./$file
         echo "$file is not a symlink";
     fi
 done
