06a3bca1ca51679b0cee5469250646d00f0c2d38
Art <art.enikoff@gmail.com>
2019-06-10 23:31:21 +0300

    Added dav checker

--- rclone_sync.sh
+++ rclone_sync.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 . ./helpers.sh
-/home/borg/rclone-folder/rclone sync -v --copy-links /backup/borg/data/ onedrive1:mother
+/home/borg/rclone-folder/rclone sync --copy-links /backup/borg/data/ onedrive1:mother
 RESULT1=$?
 if [ ${RESULT1} -ne 0 ]; then
   error_echo "Error copying to onedrive1"
 fi
 
-/home/borg/rclone-folder/rclone sync -v --copy-links /backup/borg/data/ onedrive2:father
+/home/borg/rclone-folder/rclone sync --copy-links /backup/borg/data/ onedrive2:father
 RESULT2=$?
 if [ ${RESULT2} -ne 0 ]; then
   error_echo "Error copying to onedrive2"
 fi
 
-/home/borg/rclone-folder/rclone sync -v --copy-links /backup/borg/data/ onedrive3:sister
+/home/borg/rclone-folder/rclone sync --copy-links /backup/borg/data/ onedrive3:sister
 RESULT3=$?
 if [ ${RESULT3} -ne 0 ]; then
   error_echo "Error copying to onedrive3"
 fi
 
-/home/borg/rclone-folder/rclone sync -v --copy-links /backup/borg/data/ onedrive4:brother
+/home/borg/rclone-folder/rclone sync --copy-links /backup/borg/data/ onedrive4:brother
 RESULT4=$?
 if [ ${RESULT4} -ne 0 ]; then
   error_echo "Error copying to onedrive4"
